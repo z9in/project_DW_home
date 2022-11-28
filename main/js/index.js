9e8c3d6fc7374ae2c4cc60b1d369e6561a0812f5
z9in <113665653+z9in@users.noreply.github.com>
2022-11-28 09:32:45 +0900

    Add files via upload

--- main/js/index.js
+++ main/js/index.js
@@ -7,12 +7,16 @@ $(function () {
     "main/images/main_bn_3.jpg",
   ];
 
-  let bnTitle_1 = ["Eco", "Improving", "Qulity"];
-  let bnTitle_2 = ["Plastic Company", "People Life", "Global Leader"];
+  let bnTitle_1 = ["Eco-", "Improving", "Qualified"];
+  let bnTitle_2 = [
+    "Friendly Plastic Company",
+    "People's Life",
+    "Global Leader",
+  ];
   let bnTitle_3 = [
     "대웅은 고객의 삶의 질을 높이기 위해 친환경 플라스틱에 앞장서겠습니다",
     "인류의 건강과 환경를 위한 플라스틱을 만들겠습니다  ",
-    "세계 최대 규모의 연구 네트워크를 통해 최고 수준의 자동차 내장제를 제공합니다 ",
+    "세계 최대 규모의 연구 네트워크를 통해 최고 수준의 자동차 내장재를 제공합니다 ",
   ];
 
   let bg_img_num = 0;
